8bb9952fc8cd84a34fb86ae311fc2da0d7d43edf
ketsomp <aniketpruthvi@gmail.com>
2026-02-03 12:03:31 +0530

    updated anna version

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-rm -rf *
-git clone https://github.com/ketsomp/internethome.git
-cp -r internethome/site .
-curl -L https://github.com/anna-ssg/anna/releases/download/v3.0.0/anna_Linux_x86_64.tar.gz > anna_Linux_x86.tar.gz
-tar -xvzf anna_Linux_x86.tar.gz
+ls -la
+curl -L https://github.com/anna-ssg/anna/releases/download/v4.0.0-rc-2/anna_Linux_x86_64.tar.gz > anna_Linux_x86_64.tar.gz
+tar -xvzf anna_Linux_x86_64.tar.gz
 ./anna
